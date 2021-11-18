56d37f338b917c2789e496a5ca9333d3e052d757
Michał Buńkowski <michalbunkowski@gmail.com>
2021-11-18 14:38:27 +0100

    example commit

--- packages/web/pages/index.tsx
+++ packages/web/pages/index.tsx
@@ -1,11 +1,26 @@
-import { NextPage } from 'next';
+import { GetServerSideProps, NextPage } from 'next';
 
 import { Container, Typography } from '@mui/material';
 
-export const Index: NextPage = () => {
+interface Props {
+  name: string;
+  date: string;
+}
+
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
+  return {
+    props: {
+      name: 'Michal B.',
+      date: new Date().toTimeString(),
+    },
+  };
+};
+
+export const Index: NextPage<Props> = ({ name, date }) => {
   return (
     <Container>
-      <Typography variant="h3">Index Page</Typography>
+      <Typography variant="h3">Hello {name}!</Typography>
+      <Typography variant="h5">Time is {date}</Typography>
     </Container>
   );
 };
